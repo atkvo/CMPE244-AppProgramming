348055614e74eac50b88fcf99dd96ad03aa13588
James Grantham <james.grantham@gmail.com>
2017-04-06 00:34:42 -0700

    add file append for 'stuck' tasks
    still need to check if more is required to direct a write to a particular drive such as flassh or SD. At the moment it seems that needs to be addressed but will need to examine code for more details

--- L5_Application/watchdog.cpp
+++ L5_Application/watchdog.cpp
@@ -8,11 +8,11 @@
 #include "watchdog.hpp"
 #include <stdio.h>
 #include "event_groups.h"
+#include "storage.hpp"
+#include "string.h"
 
-
-
+#define BIT_0    ( 1 << 0 )
 #define BIT_1    ( 1 << 1 )
-#define BIT_2    ( 1 << 2 )
 
 typedef enum {
     shared_eventGroupID
@@ -21,7 +21,7 @@ typedef enum {
 
 Watchdog::Watchdog(uint8_t priority) : scheduler_task("watchdog", 1024*4, priority) {
     // task runs once every second
-    setRunDuration(1000);
+    //setRunDuration(1000);
 
 }
 
@@ -33,16 +33,31 @@ bool Watchdog::run(void *p) {
     EventGroupHandle_t taskEventGroup = getSharedObject(shared_eventGroupID);
     printf("test\n");
     static EventBits_t taskBits;
+    const int maxBufferSize = 256;
+    static char stuckBuffer[maxBufferSize];
+    memset(stuckBuffer, 0, maxBufferSize);
+
+
     taskBits = xEventGroupWaitBits(
                         taskEventGroup,    // The event group being tested.
-                        BIT_1 | BIT_2,  // The bits within the event group to wait for.
-                        pdTRUE,         // BIT_0 and BIT_4 should be cleared before returning.
+                        BIT_0 | BIT_1,  // The bits within the event group to wait for.
+                        pdTRUE,         // BIT_0 and BIT_1 should be cleared before returning.
                         pdTRUE,         // Wait for both bits
                         1000 );         // Wait a maximum of 1000ms for both bit to be set.
 
-    if( ( taskBits & ( BIT_1 | BIT_2 ) ) == ( BIT_1 | BIT_2 ) )
+    if( ( taskBits & ( BIT_0 | BIT_1 ) ) != ( BIT_0 | BIT_1 ) )
     {
-        // xEventGroupWaitBits() returned because both bits were set.
+        // xEventGroupWaitBits() returned even though both bits were not set.
+        if (( taskBits & BIT_0 ) != BIT_0) {
+            strcpy(stuckBuffer,"Task 1 is stuck\n");
+        }
+        if (( taskBits & BIT_1 ) != BIT_1) {
+            strcpy(stuckBuffer,"Task 2 is stuck\n");
+        }
+        if (( taskBits & BIT_1 ) != BIT_1) {
+            strcpy(stuckBuffer,"Both tasks are stuck!\n");
+        }
+        Storage::append("stuck.txt", stuckBuffer, strlen(stuckBuffer));
     }
 
     return true;
